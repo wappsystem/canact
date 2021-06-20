064cdc29c382e389307454b136d85b3db6d14dd0
Gunnar Unger <gunnar.unger@gmail.com>
2021-06-20 16:17:45 +1000

    update

--- module-list.js
+++ module-list.js
@@ -2,6 +2,7 @@
     var modules={
         "survey-data": {url:"$H/m/survey-data.html",Table:"canact-survey",form_module:"survey-form",report:"$H/oq.html",router:1},
         "survey-form": {url:"$H/m/survey-form.html",Table:"canact-survey",report:"$H/oq.html",router:1},
+        "not_elig": {url:"$H/m/not_elig.html"},
         "report-form": {url:"$H/m/report-form.html",Table:"canact-report",task_name:"Report Details"},
         "report-form-edit": {url:"$H/m/report-form-edit.html",Table:"canact-report",task_name:"Report Details"},
         "report-canact":{url:"$H/oq.html"},
